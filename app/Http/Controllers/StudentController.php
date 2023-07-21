310b71a61de7fcdef21ce26456b766344afc238b
NouraMohamed123 <nouramohamed091@gmail.com>
2023-07-21 17:32:45 +0300

    update

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -1,8 +1,11 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use App\Models\Student;
 use DataTables;
+
 class StudentController extends Controller
 {
 
@@ -22,25 +25,25 @@ class StudentController extends Controller
             'name' => 'required|string|max:255',
             'username' => 'required|string|max:255',
             'email' => 'required|email|',
-            'phone' =>'required'
+            'phone' => 'required'
         ]);
         Student::create($validatedData);
         return redirect()->route('dashboard.students.index')->with('success', 'User created successfully.');
     }
     public function edit(Student $student)
     {
-        return view('dashboard.students.edit',compact('student'));
+        return view('dashboard.students.edit', compact('student'));
     }
 
-    public function update(Request $request,Student $student)
+    public function update(Request $request, Student $student)
     {
         $validatedData = $request->validate([
             'name' => 'required|string|max:255',
             'username' => 'required|string|max:255',
-            'email' => 'required|email|unique:students,email,'.$student->id,
-            'phone' =>'required'
+            'email' => 'required|email|unique:students,email,' . $student->id,
+            'phone' => 'required'
         ]);
-        $student->update( $validatedData);
+        $student->update($validatedData);
         return redirect()->route('dashboard.students.index')->with('success', 'User updated successfully.');
     }
     public function destroy($id)
@@ -63,14 +66,14 @@ class StudentController extends Controller
     public function getStudents(Request $request)
     {
         // if ($request->ajax()) {
-            $data = Student::latest()->get();
-          return Datatables::of($data)
-                ->addIndexColumn()
-                ->addColumn('action', function($row){
-                    $actionBtn = '<a href="'.Route('dashboard.students.edit',$row->id).'" class="edit btn btn-success btn-sm">Edit</a>
-                    <form class="delete d-inline-block" action="'.route('dashboard.students.delete').'" method="post">
-                    '.csrf_field().'
-                    <input type="hidden" name="student_id" value="'.$row->id.'">
+        $data = Student::latest()->get();
+        return Datatables::of($data)
+            ->addIndexColumn()
+            ->addColumn('action', function ($row) {
+                $actionBtn = '<a href="' . Route('dashboard.students.edit', $row->id) . '" class="edit btn btn-success btn-sm">Edit</a>
+                    <form class="delete d-inline-block" action="' . route('dashboard.students.delete') . '" method="post">
+                    ' . csrf_field() . '
+                    <input type="hidden" name="student_id" value="' . $row->id . '">
                     <button type="submit" class="btn btn-danger btn-sm deletebtn">
                         <span class="btn-label">
                             <i class="fas fa-trash"></i>
@@ -78,13 +81,11 @@ class StudentController extends Controller
                         delete
                     </button>
                 </form>';
-                    return $actionBtn;
-                })
+                return $actionBtn;
+            })
 
-                ->rawColumns(['action'])
+            ->rawColumns(['action'])
 
-                ->make(true);
-
+            ->make(true);
     }
-
 }
